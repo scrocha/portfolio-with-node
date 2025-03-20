95a1f93972ebe916f211c908a2ca8d89b0f14808
Sillas Rocha <sillasrocha29@gmail.com>
2025-03-20 12:55:13 -0300

    add base projects

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,12 +2,23 @@
   <title>Projetos</title>
 </svelte:head>
 
+<script>
+  import projects from "$lib/projects.json";
+</script>
+
   <main>
-    <article class="project">
-        <h2>1. Sistema de Ranking de Atletas de Vela</h2>
-        <p>Desenvolvi um sistema de ranking para atletas de vela em um projeto em grupo com a Confederação Brasileira de Vela. Utilizei Python com pandas e scikit-learn para analisar dados e gerar insights que melhoraram o desempenho da equipe.</p>
-        <a href="https://github.com/Gab-Mel/sailing-rankings-2024.1">Repositório do Projeto</a>    
-    </article>
+    <h1>{ projects.length } Projetsos</h1>
+    <div class="projects">
+      {#each projects as p}
+        <article>
+          <h2>{ p.title }</h2>
+          <img src={ p.image } alt="" />
+          <p>
+            { p.description }
+          </p>
+        </article>
+      {/each}
+    </div>
   </main>
   
   <footer>
