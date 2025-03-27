614f9b1ea6fad47b66ea8077f302d8b9cad83857
SILLAS ROCHA DA COSTA <B51073@fgv.edu.br>
2025-03-27 12:09:50 -0300

    que se faça a luz

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,93 @@
+
+<script>
+import { base } from "$app/paths";
+import { page } from "$app/stores";
+
+
+let pages = [
+    { url: ".", title: "Sobre Mim" },
+    { url: "projects", title: "Projetos" },
+    { url: "resume", title: "Currículo" },
+    { url: "contact", title: "Contato" },
+    { url: "https://github.com/scrocha", title: "GitHub" }
+];
+
+
+let localStorage = globalThis.localStorage ?? {};
+
+let colorScheme = localStorage.colorScheme ?? "light dark";
+
+
+let root = globalThis?.document?.documentElement;
+root?.style.setProperty("color-scheme", colorScheme);
+$: root?.style.setProperty("color-scheme", colorScheme);
+
+
+
+</script>
+
+
+<nav>
+    {#each pages as p}
+        <a
+            href={p.url.startsWith("http") ? p.url: `${base}${p.url}`}
+            class:current={$page.route.id === p.url}
+            target={p.url.startsWith("http") ? "_blank" : undefined}
+        >
+            {p.title}
+        </a>
+    {/each}
+</nav>
+
+
+<label class="color-scheme">
+    Theme:
+    <select bind:value={colorScheme}>
+        <option value="light dark"> Automatic </option>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+    </select>
+</label>
+
+
+<style>
+:root {
+  --grey: oklch(80% 3% 200);
+  --collor-accent: oklch(65% 50% 70);
+  --collor-accent-bg: oklch(65% 50% 70 / 25%);
+}
+
+nav {
+  display: flex;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: var(--grey);
+}
+
+nav a {
+  flex: 1;
+  text-decoration: none;
+  color: inherit;
+  text-align: center;
+  padding: 0.5em;
+}
+
+nav .current {
+border-bottom-width: 0.4em;
+border-bottom-style: solid;
+border-bottom-color: var(--grey);
+padding: 0.1em;
+
+}
+nav a:hover{
+border-bottom-width: 0.4em;
+border-bottom-style: solid;
+border-bottom-color: var(--collor-accent);
+padding-bottom: -0.1em;
+background-color: var(--collor-accent-bg);
+}
+
+</style>
+
+
+<slot />
--- static/global.js
+++ static/global.js
@@ -1,52 +1,52 @@
-console.log("IT’S ALIVE!");
+// console.log("IT’S ALIVE!");
 
-function $$(selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
-}
+// function $$(selector, context = document) {
+//     return Array.from(context.querySelectorAll(selector));
+// }
 
-let pages = [
-    { url: ".", title: "Sobre Mim" },
-    { url: "projects", title: "Projetos" },
-    { url: "resume", title: "Currículo" },
-    { url: "contact", title: "Contato" },
-    { url: "https://github.com/scrocha", title: "GitHub" }
-];
+// let pages = [
+//     { url: ".", title: "Sobre Mim" },
+//     { url: "projects", title: "Projetos" },
+//     { url: "resume", title: "Currículo" },
+//     { url: "contact", title: "Contato" },
+//     { url: "https://github.com/scrocha", title: "GitHub" }
+// ];
 
-// Busca os links já existentes no <nav> (caso existam)
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+// // Busca os links já existentes no <nav> (caso existam)
+// // let navLinks = $$("nav a");
+// // let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
 
-if (currentLink) {
-    currentLink.classList.add("current");
-}
+// if (currentLink) {
+//     currentLink.classList.add("current");
+// }
 
-let h1 = document.createElement("h1");
-h1.textContent = document.title;
-document.body.prepend(h1);
+// let h1 = document.createElement("h1");
+// h1.textContent = document.title;
+// document.body.prepend(h1);
 
-let nav = document.createElement("nav");
-document.body.insertBefore(nav, h1.nextSibling);
+// let nav = document.createElement("nav");
+// document.body.insertBefore(nav, h1.nextSibling);
 
-for (let p of pages) {
-    let url = p.url;
-    let title = p.title;
+// // for (let p of pages) {
+// //     let url = p.url;
+// //     let title = p.title;
     
-    // if (!url.startsWith("http") && !url.startsWith("/")) {
-    //     url = "../" + url;
-    // }
+// //     // if (!url.startsWith("http") && !url.startsWith("/")) {
+// //     //     url = "../" + url;
+// //     // }
     
-    let ref = document.createElement("a");
-    ref.href = url;
-    ref.textContent = title;
+// //     let ref = document.createElement("a");
+// //     ref.href = url;
+// //     ref.textContent = title;
 
-    if (ref.host === location.host && ref.pathname === location.pathname) {
-        ref.classList.add("current");
-    }
+// //     if (ref.host === location.host && ref.pathname === location.pathname) {
+// //         ref.classList.add("current");
+// //     }
     
-    // Links externos abrem em nova aba
-    if (ref.host !== location.host) {
-        ref.target = "_blank";
-    }
+// //     // Links externos abrem em nova aba
+// //     if (ref.host !== location.host) {
+// //         ref.target = "_blank";
+// //     }
     
-    nav.append(ref);
-}
+// //     nav.append(ref);
+// // }
